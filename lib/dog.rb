5bee0c6a6d49615e7d7883a3591f2601faeb95a2
Christopher Chaison <cchaison0@gmail.com>
2020-03-14 15:27:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def print_all
+    puts @@all.collect{|dog| dog.name}.join(" ")
+  end
+
+  def save
+    @@all << self
+  end
 end
